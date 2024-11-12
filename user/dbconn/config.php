968bbdbdc98972bb36f195c9af11e7a06cb10773
wendhil himarangan <Wendhil>
2024-11-12 18:36:35 +0800

    little error

--- user/dbconn/config.php
+++ user/dbconn/config.php
@@ -1,15 +1,8 @@
 <?php
-<<<<<<< HEAD
   define('DB_HOST','localhost:3306');
   define('DB_USER','root');
   define('DB_PASSWORD','');
   define('DB_NAME','test');
-=======
-  define('DB_HOST','localhost');
-  define('DB_USER','bpa_db');
-  define('DB_PASSWORD','oLtxJc8lIy+djioy');
-  define('DB_NAME','bpa_system');
->>>>>>> 613bf9cbb8b003106be1579d4b6707d73c148df4
 
 
    $conn = new mysqli(DB_HOST,DB_USER,  DB_PASSWORD, DB_NAME);
@@ -18,7 +11,4 @@
       die("Conneciton failed:".$conn->connect_error);
    }
 
-<<<<<<< HEAD
    //bpa_db,oLtxJc8lIy+djioy,bpa_system
-=======
->>>>>>> 613bf9cbb8b003106be1579d4b6707d73c148df4
